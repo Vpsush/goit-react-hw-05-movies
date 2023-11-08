625fd3c9cd82b4740a180e00ad26d162239f80cc
Vpsush <vpsush2023@gmail.com>
2023-11-09 00:00:35 +0200

    2

--- src/pages/MoviesPageDetails.jsx
+++ src/pages/MoviesPageDetails.jsx
@@ -1,6 +1,6 @@
 // import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { useEffect } from 'react';
 
 // const MoviesPageDetails = () => {
 //   const { movieId } = useParams();

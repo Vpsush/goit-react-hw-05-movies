10b2343b48eeca95ada7b7999fcd245e94b98790
Vpsush <vpsush2023@gmail.com>
2023-11-10 00:05:15 +0200

    2

--- src/pages/PageActors.jsx
+++ src/pages/PageActors.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Cast = () => {
-  return <div>HomePage</div>;
+  const { moviesId } = useParams;
+  return <div>reviews:{moviesId}</div>;
 };
 
 export default Cast;

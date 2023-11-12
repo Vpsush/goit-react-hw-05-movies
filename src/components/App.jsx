923ef7e66f56881422d0dea619915e20b11ef466
Vpsush <vpsush2023@gmail.com>
2023-11-12 21:04:47 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,18 @@
-// import { NavLink, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 // import Home from 'pages/HomePage';
 // import Movies from 'pages/MoviesPage';
 // import MoviesPageDetails from 'pages/MoviesPageDetails';
-// import Cast from 'pages/PageActors';
-// import Reviews from 'pages/PageRewies';
-// import { StyledMovies } from './Movies.styled';
-
-// export const App = () => {
-//   return (
-//     <StyledMovies>
-//       <div>
-//         <ul className="title">
-//           <li>
-//             <NavLink to="/">Home</NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="/movies">Movies</NavLink>
-//           </li>
-//           {/* <NavLink to="/movies/:movieId">Movies details</NavLink>
-//         <NavLink to="/movies/:movieId/cast">Cast</NavLink>
-//         <NavLink to="/movies/:movieId/reviews">Reviews</NavLink> */}
-//         </ul>
-//         <main>
-//           <Routes>
-//             <Route path="/" element={<Home />}></Route>
-//             <Route path="/movies" element={<Movies />}>
-//               <Route path=":movieId/cast" element={<Cast />}></Route>
-//               <Route path=":movieId/reviews" element={<Reviews />}></Route>
-//             </Route>
-//             <Route
-//               path="/movies/:movieId"
-//               element={<MoviesPageDetails />}
-//             ></Route>
-//           </Routes>
-//         </main>
-//       </div>
-//     </StyledMovies>
-//   );
-// };
-
-import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
-import Home from 'pages/HomePage';
-import Movies from 'pages/MoviesPage';
-import MoviesPageDetails from 'pages/MoviesPageDetails';
-import Cast from 'pages/Cast';
-import Reviews from 'pages/Reviews';
+// import Cast from 'pages/Cast';
+// import Reviews from 'pages/Reviews';
 import { StyledMovies } from '../Movies.styled';
+import Loader from './Loader/Loader';
+
+const Home = lazy(() => import('pages/HomePage'));
+const Movies = lazy(() => import('pages/MoviesPage'));
+const Reviews = lazy(() => import('pages/Reviews'));
+const Cast = lazy(() => import('pages/Cast'));
+const MoviesPageDetails = lazy(() => import('pages/MoviesPageDetails'));
 
 export const App = () => {
   return (
@@ -60,15 +27,17 @@ export const App = () => {
           </li>
         </ul>
         <main>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/movies" element={<Movies />}></Route>
-            <Route path="/movies/:movieId" element={<MoviesPageDetails />}>
-              <Route path="/movies/:movieId/cast" element={<Cast />} />
-              <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/movies" element={<Movies />}></Route>
+              <Route path="/movies/:movieId" element={<MoviesPageDetails />}>
+                <Route path="/movies/:movieId/cast" element={<Cast />} />
+                <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+              </Route>
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </StyledMovies>

01ce2b8d5ae02afcb13d8ffac786fd098f419e01
Vpsush <vpsush2023@gmail.com>
2023-11-17 00:01:32 +0200

    33

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 // import MoviesPageDetails from './MoviesPageDetails';
 import axios from 'axios';
 import { API_KEY, BASE_URL } from 'additional/const';
